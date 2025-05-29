f3005424a61096a41b46dc05c76e8e296cfaf050
Mats Estensen <matsest@mxe.no>
2025-05-29 13:38:30 +0200

    fix: write to host

--- main.ps1
+++ main.ps1
@@ -14,14 +14,22 @@ $userAccessToken = Get-SpotifyAccessTokenByAuthorizationCode -AuthorizationCode 
 
 # Right-click on the playlist > Share > Copy Share Link. The link will contain the playlist ID. It is the string between playlist/ and ?si=.
 $playlistId = "<your_playlist_id_here>"  # Replace with your actual playlist ID
+$playlist = Get-SpotifyPlaylist -PlaylistId $playlistId -AccessToken $userAccessToken
+Write-Host "Playlist Name: $($playlist.name)"
 
 $alltracks = Get-SpotifyPlaylistTracks -PlaylistId $playlistId -AccessToken $userAccessToken
 
 $numberOfTracks = $alltracks.Count
+Write-Host "Number of tracks in the playlist: $numberOfTracks"
 $numberOfArtists = ($alltracks.track.artists.name | Select-Object -Unique).Count
+Write-Host "Number of unique artists in the playlist: $numberOfArtists"
+$artistsOnlyPlayedOnce = $alltracks.track.artists.name | Group-Object | Where-Object { $_.Count -eq 1 } | Select-Object -ExpandProperty Name
+Write-Host "Number of artists played only once in the playlist: $($artistsOnlyPlayedOnce.Count)"
 
 $top50Artists = $alltracks.track.artists.name | Group-Object | Sort-Object -Property Count -Descending | Select-Object -First 50 Count, Name
-$artistsOnlyPlayedOnce = $alltracks.track.artists.name | Group-Object | Where-Object { $_.Count -eq 1 } | Select-Object -ExpandProperty Name
+# print
+Write-Host "Top 50 artists in the playlist:"
+Write-Host ($top50Artists | Out-String)
 
 $top50SongsByPopularity = $alltracks.track | Sort-Object id | Get-Unique -AsString | Sort-Object -Property popularity -Descending | Select-Object -First 50 | ForEach-Object {
     [PSCustomObject]@{
@@ -30,6 +38,8 @@ $top50SongsByPopularity = $alltracks.track | Sort-Object id | Get-Unique -AsStri
         Artist     = ($_.artists | ForEach-Object { $_.name }) -join ', '
     }
 }
+Write-Host "Top 50 songs by popularity in the playlist:"
+Write-Host ($top50SongsByPopularity | Out-String)
 
 # Note use name and artists to group tracks, as some tracks have different ids but are the same song (e.g. remixes)
 $tracksPlayedMoreThanOnce = $alltracks.track | Group-Object name, artists | Where-Object { $_.Count -gt 1 } | ForEach-Object {
@@ -39,3 +49,5 @@ $tracksPlayedMoreThanOnce = $alltracks.track | Group-Object name, artists | Wher
         Artists = ($_.Group | ForEach-Object { $_.artists.name } | Select-Object -Unique) -join ', '
     }
 } | Sort-Object -Property Count -Descending
+Write-Host "Tracks played more than once in the playlist:"
+Write-Host ($tracksPlayedMoreThanOnce | Out-String)
